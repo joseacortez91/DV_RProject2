67d0655411fed5746bd23545635e79e6a67fef0d
Jose Cortez <joseacortez91ut@gmail.com>
2015-02-10 18:17:47 -0600

    created the exoplanet dataframe

---
+++ 01 Data/create-df.R
@@ -0,0 +1,1 @@
+df <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from exoplanets"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jac5699', PASS='orcl_jac5699', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
