250f2aa55d2c4f68487a7f20855a34e727ac8fb2
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-02-11 08:57:19 -0600

    Created radial.df

--- 01 Data/create-df.R
+++ 01 Data/create-df.R
@@ -1,1 +1,5 @@
 df <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from exoplanets"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jac5699', PASS='orcl_jac5699', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
+
+
+#Subsetting data to incluce only those exoplanets with a radius
+radial.df <- subset(df, RADIUS != 'null')
