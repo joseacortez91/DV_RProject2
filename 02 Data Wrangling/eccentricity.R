b1a00353858e582088549c45403e6976a8fffbc4
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-02-14 16:07:33 -0600

    Added Second Analysis Of Data Though, I'm not really sure what these graphs say, here they are! I mean, they look interesting at least.

---
+++ 02 Data Wrangling/eccentricity.R
@@ -0,0 +1,4 @@
+eccen.df <- filter(df, RADIUS != 'null' & MASS != 'null' & ORB_PERIOD != 'null' & ECCENTRICITY != 'null') %>% mutate(SUN = sub(".$","",NAME, perl = TRUE)) %>% select(SUN, ECCENTRICITY, ORB_PERIOD, MASS) %>% group_by(SUN) %>% summarise(Mean_Eccentricty = mean(ECCENTRICITY), Mean_Orb_Period = mean(ORB_PERIOD), Mean_mass = mean(MASS), Planets = n()) %>% filter(Planets >1) %>% mutate(Mass_Cume_Dist = cume_dist(Mean_mass)) %>% mutate(Mass=ntile(Mass_Cume_Dist,4))
+
+eccen2.df <- filter(df, RADIUS != 'null' & MASS != 'null' & ORB_PERIOD != 'null' & ECCENTRICITY != 'null') %>% mutate(SUN = sub(".$","",NAME, perl = TRUE)) %>% select(SUN, ECCENTRICITY, ORB_PERIOD, MASS) %>% group_by(SUN) %>% summarise(Mean_Eccentricty = mean(ECCENTRICITY), Mean_Orb_Period = mean(ORB_PERIOD), Mean_mass = mean(MASS), Planets = n()) %>% mutate(Mass_Cume_Dist = cume_dist(Mean_mass)) %>% mutate(Mass=ntile(Mass_Cume_Dist,4))
+
