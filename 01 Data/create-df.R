e42a4771a0d15bd73004968c02d60aa8db663a1f
Jose Cortez <joseacortez91ut@gmail.com>
2015-02-16 15:30:45 -0600

    Created new dataframe to compare exoplanets to Earth. As soon as Adam fixes the numeric problem I'll continue working on this dataframe

--- 01 Data/create-df.R
+++ 01 Data/create-df.R
@@ -1,6 +1,6 @@
 df <- data.frame(eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from exoplanets"'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_jac5699', PASS='orcl_jac5699', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'False'), verbose = TRUE), 1, 2^31-1))))
 
-#Subsetting data to incluce only those exoplanets with a radius
+#Subsetting data to include only those exoplanets with a radius
 radial.df <- filter(df, RADIUS != 'null' & MASS != 'null' & ORB_PERIOD != 'null') %>%
   arrange(desc(ECCENTRICITY))
 
