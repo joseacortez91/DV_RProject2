10634e140abf092a95445bc516b252e6aac0ef19
Alex Pearce <alexpearce92@gmail.com>
2015-02-16 19:12:09 -0600

    Added units to mass

--- 03 Visualizations/mass_radius_analysis.R
+++ 03 Visualizations/mass_radius_analysis.R
@@ -1,1 +1,1 @@
-massAndRadius.df  %>% ggplot(aes(x=MASS_PERCENTILE, y=AVG_MASS, color=AVG_RADIUS))+geom_point(na.rm=TRUE)
+massAndRadius.df  %>% ggplot(aes(x=MASS_PERCENTILE, y=AVG_MASS, color=AVG_RADIUS))+geom_point(na.rm=TRUE) + scale_y_continuous(name="Average Mass (Jupiters)")
